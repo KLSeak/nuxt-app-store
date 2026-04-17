b75c201acb7b1c88afe58271240c6edc77ac840b
KLSeak <leangseakkong@gmail.com>
2026-04-17 19:06:29 +0700

    first commit

---
+++ app/pages/apps/dashboard/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { f } from 'vue-router/dist/router-CWoNjPRp.mjs';
+
+definePageMeta({ 
+    layout: "dashboard",
+    requiresAuth: true,
+})
+</script>
+
+<template>
+    <h2>
+        Hello from index.vue file
+    </h2>
+</template>
